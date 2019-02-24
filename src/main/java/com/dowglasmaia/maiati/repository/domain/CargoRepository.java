c50ed2ba5cc22d09aef171562eaf0cc106f2790e
Dowglas Maia <dowglasmaia@gmail.com>
2019-02-24 19:29:29 -0300

    Criando Repositorios das Entidades

---
+++ src/main/java/com/dowglasmaia/maiati/repository/domain/CargoRepository.java
@@ -0,0 +1,11 @@
+package com.dowglasmaia.maiati.repository.domain;
+
+import org.springframework.stereotype.Repository;
+
+import com.dowglasmaia.maiati.domain.Cargo;
+import com.dowglasmaia.maiati.repository.AbstractRepo;
+
+@Repository
+public class CargoRepository extends AbstractRepo<Cargo, Long>{
+
+}
---
+++ src/main/java/com/dowglasmaia/maiati/repository/domain/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package com.dowglasmaia.maiati.repository.domain;
+
+import org.springframework.stereotype.Repository;
+
+import com.dowglasmaia.maiati.domain.Funcionario;
+import com.dowglasmaia.maiati.repository.AbstractRepo;
+
+@Repository
+public class FuncionarioRepository extends AbstractRepo<Funcionario, Long> {
+
+}
