e40add5c823e5470ae1cd3ccdb1b331ec352c72e
Dowglas Maia <dowglasmaia@gmail.com>
2019-03-02 15:26:10 -0300

    Impl-> Endponts, Salvar, Editar, Buscar_por_nome, e Delete - de Cargo

--- src/main/java/com/dowglasmaia/maiati/restcontroller/CargoController.java
+++ src/main/java/com/dowglasmaia/maiati/restcontroller/CargoController.java
@@ -1,13 +1,22 @@
 package com.dowglasmaia.maiati.restcontroller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dowglasmaia.maiati.domain.Cargo;
 import com.dowglasmaia.maiati.domain.dto.CargoDTO;
@@ -16,17 +25,59 @@ import com.dowglasmaia.maiati.service.CargoService;
 @RestController
 @RequestMapping("/cargos")
 public class CargoController {
-	
+
 	@Autowired
 	private CargoService service;
-	
+
 	/* Endpoint - Listar Todos */
 	@GetMapping
 	public ResponseEntity<List<CargoDTO>> listarTodos() {
 		List<Cargo> lista = service.buscarTodos();
-		List<CargoDTO>listaDTO = lista.stream().map(obj -> new CargoDTO(obj)).collect(Collectors.toList());
+		List<CargoDTO> listaDTO = lista.stream().map(obj -> new CargoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listaDTO);
 	}
 
-	
+	/* Endpoint - Buscar por ID */
+	@GetMapping("/{id}")
+	public ResponseEntity<Cargo> buscarPorID(@PathVariable Long id) {
+		try {
+			Cargo obj = service.buscarPorID(id);
+			return ResponseEntity.ok().body(obj);
+		} catch (Exception e) {
+			throw new NoSuchElementException("Registro não encontrado");
+		}
+
+	}
+
+	/* Endpoint - Salvar */
+	@PostMapping
+	public ResponseEntity<Void> save(@Valid @RequestBody Cargo cargo) {
+		Cargo obj = service.salvar(cargo);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
+	}
+
+	/* Endpoint - Buscar por nome */
+	@GetMapping("lista/{nome}")
+	public ResponseEntity<List<Cargo>> buscarPorNome(@PathVariable String nome) {
+		try {
+			List<Cargo> lista = service.buscarPorNome(nome);
+			return ResponseEntity.ok().body(lista);
+		} catch (Exception e) {
+			throw new NoSuchElementException("Registro não encontrado");
+		}
+
+	}
+
+	/* Endpoint - Delete */
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		try {
+			service.remove(id);
+		} catch (Exception e) {
+			throw new NoSuchElementException("O Operação Não realizada!");
+		}
+	}
+
 }
