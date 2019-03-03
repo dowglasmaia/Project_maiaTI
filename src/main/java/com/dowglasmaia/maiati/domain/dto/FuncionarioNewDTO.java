dc10ff90a87c1a6397aa45a3a1e078b23567143c
Dowglas Maia <dowglasmaia@gmail.com>
2019-03-03 15:03:41 -0300

    Criando FuncionarioDTO

---
+++ src/main/java/com/dowglasmaia/maiati/domain/dto/FuncionarioNewDTO.java
@@ -0,0 +1,158 @@
+package com.dowglasmaia.maiati.domain.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class FuncionarioNewDTO {
+
+	private Long id;
+
+	private String nome;
+
+	private String email;
+
+	private String telefone;
+
+	private LocalDate dataAdmissao;
+
+	private LocalDate dataDemissao;
+
+	private BigDecimal salario;
+
+	private String cargo;
+
+	private String logradouro;
+
+	private String numero;
+
+	private String bairro;
+
+	private String cep;
+
+	private String complemento;
+
+	private Long cidadeID;
+
+	private Long estadoID;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public LocalDate getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public void setDataAdmissao(LocalDate dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public LocalDate getDataDemissao() {
+		return dataDemissao;
+	}
+
+	public void setDataDemissao(LocalDate dataDemissao) {
+		this.dataDemissao = dataDemissao;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public Long getCidadeID() {
+		return cidadeID;
+	}
+
+	public void setCidadeID(Long cidadeID) {
+		this.cidadeID = cidadeID;
+	}
+
+	public Long getEstadoID() {
+		return estadoID;
+	}
+
+	public void setEstadoID(Long estadoID) {
+		this.estadoID = estadoID;
+	}
+
+}
