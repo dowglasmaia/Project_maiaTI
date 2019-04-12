745d463dce581ebdbd2d1abb3e459145ca255abb
Dowglas Maia <dowglasmaia@gmail.com>
2019-04-12 13:37:38 -0300

    impl-> Autenticação e Geração de Token JWT e Spring Security

--- src/main/java/com/dowglasmaia/maiati/domain/Funcionario.java
+++ src/main/java/com/dowglasmaia/maiati/domain/Funcionario.java
@@ -3,18 +3,14 @@ package com.dowglasmaia.maiati.domain;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -25,9 +21,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @author Dowglas Maia Skype: live:dowglasmaia E-mail:dowglasmaia@live.com
